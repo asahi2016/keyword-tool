471ce059102b4705ea735b58eded73271fee6f50
avinash <avinash@asahitechnologies.com>
2016-07-04 17:05:06 +0530

    KTT-25
    Added Main.php controller
    
    Changed Default Controller to Main

---
+++ application/controllers/Main.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 4/7/16
+ * Time: 5:01 PM
+ */
+class Main extends CI_Controller
+{
+
+
+    public function index(){
+        echo "Hello World";
+    }
+}
